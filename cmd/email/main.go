6677797b72f360c6bf11078e079ae10d3410fd2c
csvitor-dev <csvitor.dev@gmail.com>
2025-06-18 15:47:02 -0300

    feat: create application entrypoint

--- cmd/email/main.go
+++ cmd/email/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/csvitor-dev/mail-sender/internal/entities"
+	"github.com/csvitor-dev/mail-sender/internal/services"
+	"github.com/csvitor-dev/mail-sender/pkg/config"
+	src_service "github.com/csvitor-dev/mail-sender/src/services"
+)
+
+func main() {
+	smtpConfig := config.NewSMTP()
+	numWorkers := 2
+
+	jobs := make(chan entities.EmailJob, 100)
+	wg := src_service.StartWorkerPool(numWorkers, jobs, smtpConfig)
+
+	services.LoadFiles(jobs)
+	close(jobs)
+	wg.Wait()
+
+	log.Println("All emails have been processed!")
+	log.Printf("Total Requests: %d\n", src_service.TotalRequests)
+	log.Printf("Fail email requests: %v\n", src_service.FailRequests)
+	log.Printf("Success email requests: %v\n", src_service.SuccessRequests)
 }
