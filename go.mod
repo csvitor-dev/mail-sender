362b87ee9967070e0321da641635802fd6c89102
csvitor-dev <csvitor.dev@gmail.com>
2025-06-18 14:01:07 -0300

    feat: add packages and get enviroment variables

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/csvitor-dev/mail-sender
 
 go 1.24.0
+
+require (
+	github.com/cstockton/go-conv v1.0.0
+	github.com/joho/godotenv v1.5.1
+	gopkg.in/mail.v2 v2.3.1
+)
+
+require gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
--- pkg/config/env.go
+++ pkg/config/env.go
@@ -1,2 +1,31 @@
 package config
 
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/cstockton/go-conv"
+	env "github.com/joho/godotenv"
+)
+
+type Enviroment struct {
+	SMTP     string
+	Port     int
+	Sender   string
+	Password string
+}
+
+var Env Enviroment
+
+func LoadEnv() {
+	if err := env.Load(); err != nil {
+		log.Fatal(err)
+	}
+
+	Env.SMTP = os.Getenv("EMAIL_SMTP")
+	Env.Port, _ = conv.Int(os.Getenv("EMAIL_PORT"))
+	Env.Sender = os.Getenv("EMAIL_SENDER")
+	Env.Password = os.Getenv("EMAIL_PASSWORD")
+	fmt.Println(Env)
+}
