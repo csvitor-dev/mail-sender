34cd3f212e70c085832f3f2704d29b66881fa88c
csvitor-dev <csvitor.dev@gmail.com>
2025-06-18 15:43:58 -0300

    feat: create services to handle files

---
+++ internal/services/file_loader.go
@@ -0,0 +1,38 @@
+package services
+
+import (
+	"github.com/csvitor-dev/mail-sender/internal/entities"
+)
+
+func LoadFiles(jobs chan<- entities.EmailJob) error {
+	contentFromCC, err := readFile("cc.txt")
+
+	if err != nil {
+		return err
+	}
+	contentFromES, err := readFile("es.txt")
+
+	if err != nil {
+		return err
+	}
+	bodyMessage := "Segue em anexo a primeira prova de Matemática Discreta.\n\nAtenciosamente,\nVitor Costa de Sousa"
+
+	for _, record := range contentFromCC {
+		fileToAttach, err := GetAbsolutePath("internal/files/cc/", record[1])
+
+		if err != nil {
+			return err
+		}
+		jobs <- entities.NewEmail(record[0], "Prova de Matemática Discreta [CC 2025.1]", bodyMessage, fileToAttach)
+	}
+
+	for _, record := range contentFromES {
+		fileToAttach, err := GetAbsolutePath("internal/files/es/", record[1])
+
+		if err != nil {
+			return err
+		}
+		jobs <- entities.NewEmail(record[0], "Prova de Matemática Discreta [ES 2025.1]", bodyMessage, fileToAttach)
+	}
+	return nil
+}
---
+++ internal/services/file_reader.go
@@ -0,0 +1,44 @@
+package services
+
+import (
+	"bufio"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func GetAbsolutePath(target, fileName string) (string, error) {
+	filePath := target + fileName
+	absolutePath, err := filepath.Abs(filePath)
+
+	if err != nil {
+		return "", err
+	}
+	return absolutePath, nil
+}
+
+func readFile(fileName string) ([][]string, error) {
+	path, err := GetAbsolutePath("internal/emails/", fileName)
+
+	if err != nil {
+		return nil, err
+	}
+	file, err := os.Open(path)
+
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	var content [][]string
+	for scanner.Scan() {
+		emailAndFile := strings.Split(scanner.Text(), ",")
+		content = append(content, emailAndFile)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+	return content, nil
+}
