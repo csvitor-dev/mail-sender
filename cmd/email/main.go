e24c01a41fc77ac284c2e6a5370c7aec17e89a24
csvitor-dev <csvitor.dev@gmail.com>
2025-08-05 17:49:59 -0300

    refactor: improves email sender entrypoint

--- cmd/email/main.go
+++ cmd/email/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"log"
+	"strings"
 
 	"github.com/csvitor-dev/mail-sender/internal/entities"
-	"github.com/csvitor-dev/mail-sender/internal/services"
+	"github.com/csvitor-dev/mail-sender/internal/services/files"
+	"github.com/csvitor-dev/mail-sender/pkg/cli"
 	"github.com/csvitor-dev/mail-sender/pkg/config"
 	src_service "github.com/csvitor-dev/mail-sender/src/services"
 )
@@ -13,10 +15,18 @@ func main() {
 	smtpConfig := config.NewSMTP()
 	numWorkers := config.Env.WORKER_NUM
 
+	courseAndProofNum := strings.ToLower(cli.Prompt("Enter the target course (CC or ES) with number (1, 2, 3)"))
+
+	if courseAndProofNum[:2] != "cc" && courseAndProofNum[:2] != "es" {
+		log.Fatalf("Invalid course: %s. Please enter 'CC' or 'ES'.", courseAndProofNum)
+	}
+	subject := cli.Prompt("Enter the subject for the emails")
+	bodyMessage := cli.Prompt("Enter the body message for the emails")
+
 	jobs := make(chan *entities.EmailJob, 100)
 	wg := src_service.StartWorkerPool(numWorkers, jobs, smtpConfig)
 
-	services.LoadFiles(jobs)
+	files.LoadFromFiles(courseAndProofNum, subject, bodyMessage, jobs)
 	close(jobs)
 	wg.Wait()
 
